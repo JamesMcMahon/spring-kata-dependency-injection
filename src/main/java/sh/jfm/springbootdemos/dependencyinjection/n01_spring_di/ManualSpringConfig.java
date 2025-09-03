622ff4016a6ec24018e18124b8f824e8d559e606
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-09-03 11:21:08 -0500

    Describe manual injection Kata

--- src/main/java/sh/jfm/springbootdemos/dependencyinjection/n01_spring_di/ManualSpringConfig.java
+++ src/main/java/sh/jfm/springbootdemos/dependencyinjection/n01_spring_di/ManualSpringConfig.java
@@ -3,6 +3,12 @@ package sh.jfm.springbootdemos.dependencyinjection.n01_spring_di;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/// Spring can handle construction of beans / class instances for you, but it is also possible to manually initialize
+/// instances and let Spring manage them for you. This can give you more explicit control over beans, and in some cases
+/// is needed when Spring needs help resolving ambiguous situations.
+///
+/// For this Kata, starting with the more explicit workflow allows us to better understand what Spring
+/// is doing automatically for future Katas.
 @Configuration
 public class ManualSpringConfig {
 
--- src/test/java/sh/jfm/springbootdemos/dependencyinjection/n01_spring_di/N01SpringDependencyInjectionTest.java
+++ src/test/java/sh/jfm/springbootdemos/dependencyinjection/n01_spring_di/N01SpringDependencyInjectionTest.java
@@ -8,6 +8,14 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/// Spring handles dependency injection for you.
+/// As we saw in the last kata, for toy or smaller projects dependency injection can be done manually.
+/// But for larger projects, manual dependency injection hits a lot of issues, and you often will end up writing
+/// your own piecemeal dependency injection framework. Spring can handle this heavy lifting for you.
+///
+/// For the best understanding, a good place to start is to look at what Spring is doing in a less automatic and magical way.
+/// The [ManualSpringConfig] is using annotations to make class instances as Spring beans. This gives Spring the ability
+/// to manage said beans and inject them into other classes.
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = ManualSpringConfig.class)
 class N01SpringDependencyInjectionTest {
