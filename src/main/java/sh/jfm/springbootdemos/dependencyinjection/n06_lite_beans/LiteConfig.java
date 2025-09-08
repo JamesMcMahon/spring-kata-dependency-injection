5b0353128db1dd1fa5a87f7106ead4f99ea59a54
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-09-08 15:13:04 -0500

    Clean up Lite mode Kata

---
+++ src/main/java/sh/jfm/springbootdemos/dependencyinjection/n06_lite_beans/LiteConfig.java
@@ -0,0 +1,25 @@
+package sh.jfm.springbootdemos.dependencyinjection.n06_lite_beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/// The use of [Component] instead of [org.springframework.context.annotation.Configuration] causes this config to
+/// be process beans in Lite mode. In Lite mode, [Bean] methods are treated as plain old Java factory methods.
+///
+/// I expect that the [Turkey] instance inside the [Duck] will be distinct from the Turkey bean.
+///
+/// **Hint: The methods in this config will behave like a standard Java factory method**
+@Component
+public class LiteConfig {
+    @Bean
+    public Turkey turkey() {
+        return new Turkey(null);
+    }
+
+    /// You can inject Turkey as a parameter, but for the purposes of this Kata, I would avoid this approach as it
+    /// avoids demonstrating the differences between the two modes.
+    @Bean
+    public Duck duck() {
+        return new Duck(null);
+    }
+}
--- src/test/java/sh/jfm/springbootdemos/dependencyinjection/n06_lite_beans/N06LiteBeansTest.java
+++ src/test/java/sh/jfm/springbootdemos/dependencyinjection/n06_lite_beans/N06LiteBeansTest.java
@@ -4,14 +4,24 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+/// Spring's [Bean] have two distinct ways that can be processed by the framework. One is normal or what I am calling
+/// "Full" mode, and the other mode is referred to by Spring as "Lite" mode.
+///
+/// Lite mode beans are processed differently. Rather than being full singletons, they can under some circumstances
+/// be created multiple times. The full [Bean] Javadocs give a more in-depth explanation of Lite mode.
+///
+/// It's worth being aware of this mode as it can lead to some interesting scenarios when debugging.
 @ExtendWith(SpringExtension.class)
 public class N06LiteBeansTest {
+
+    /// Full mode is what you are going to see in most projects
     @Nested
     @ContextConfiguration(classes = FullConfig.class)
     class FullTest {
@@ -29,8 +39,9 @@ public class N06LiteBeansTest {
         }
     }
 
+    /// Lite mode is more unusual - the key difference is the config file is not proxied by Spring
     @Nested
-    @ContextConfiguration(classes = LiteModeConfig.class)
+    @ContextConfiguration(classes = LiteConfig.class)
     class LiteTest {
         @Autowired
         private Turkey turkey;
