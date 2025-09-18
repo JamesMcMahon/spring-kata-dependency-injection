d0dc7fd59b404304a1e4f53d98efc689d2f4c3fa
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-09-18 10:07:13 -0500

    Grammar

--- src/test/java/sh/jfm/springbootdemos/dependencyinjection/n04_qualified_beans/N04QualifiedBeansTest.java
+++ src/test/java/sh/jfm/springbootdemos/dependencyinjection/n04_qualified_beans/N04QualifiedBeansTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/// In this kata there are more than one [Flavor] bean available, so we will have to be explicit about which one to use.
+/// In this kata there is more than one [Flavor] bean available, so we will have to be explicit about which one to use.
 /// This can be done with manual configuration, but in this case lets solve it while still using automatic configuration.
 /// Spring has the ability to select a specific bean using [org.springframework.beans.factory.annotation.Qualifier],
 /// which can help it work through some ambiguous situations.
